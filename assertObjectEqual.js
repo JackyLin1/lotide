70a99bd5a1b0af3aa0757865491d50ede7716aa6
JackyLin1 <jlin9702@gmail.com>
2022-01-13 06:37:13 +0000

    check if object are right with each other

--- assertObjectEqual.js
+++ assertObjectEqual.js
@@ -1,10 +1,10 @@
 const eqArrays = function(arr1,arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      return false
+      return false;
     }
   }
-  return arr1.length === arr2.length ? true : false
+  return arr1.length === arr2.length ? true : false;
 };
 
 const eqObjects = function(object1, object2) {
