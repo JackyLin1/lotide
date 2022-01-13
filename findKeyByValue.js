b57278070d09ed070f32a63768197b473b989f0d
JackyLin1 <jlin9702@gmail.com>
2022-01-13 04:32:07 +0000

    find object key through inputing its content.

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😀😀😀 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// function for find key by give object and value
+const findKeyByValue = function(obj, value) {
+  let keys = Object.keys(obj);
+  for (let key of keys) {
+    if (obj[key] === value) {
+      return key;
+    }
+  }
+};
+
+
+//test statements
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
