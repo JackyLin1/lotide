1733058c73697311f61ca2a50da87690875f50ff
JackyLin1 <jlin9702@gmail.com>
2022-01-14 02:06:58 +0000

    program will run until truthy

---
+++ takeUntil.js
@@ -0,0 +1,42 @@
+const eqArrays = function(arr1,arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return arr1.length === arr2.length ? true : false;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual,expected)) {
+    console.log(`😀😀😀 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const takeUntil = function(array, callback) {
+  result = [];
+  for (let data of array) {
+    
+    if (callback(data) === true) {
+      break;
+    }
+    
+    result.push(data);
+  }
+  return result;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+assertArraysEqual(results1,[ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
