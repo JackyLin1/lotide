f5cf99a5c830266335139608ed238467d1f5d252
JackyLin1 <jlin9702@gmail.com>
2022-01-18 09:17:14 +0900

    added require call

--- assertEqual.js
+++ assertEqual.js
@@ -7,6 +7,6 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+//Exporting for other js to use
+module.exports = assertEqual;
