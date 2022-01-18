a52de346c98386e41d126854d3001dd0177f51db
JackyLin1 <jlin9702@gmail.com>
2022-01-18 10:10:48 +0900

    refactored tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,31 @@
-const middle = require ('../middle')
+const middle = require ('../middle');
+const assert = require('chai').assert;
 
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
-console.log(middle([1])); // => []
+
+describe("#head", () => {
+
+it("returns [] for [1, 2]", () => {
+  middle([1,2]);
+});
+
+it("returns [2] for [1,2,3]", () => {
+  middle([1,2,3]);
+});
+
+it("returns [3] for [1, 2, 3, 4, 5]", () => {
+  middle([1,2,3,4,5]);
+});
+
+it("returns [2,3] for [1,2,3,4]", () => {
+  middle([1,2,3,4]);
+});
+
+it("returns [3,4] for [1,2,3,4,5,6]", () => {
+  middle([1,2,3,4,5,6]);
+});
+
+it("returns [] for [1]", () => {
+  middle([1]);
+});
+
+});
--- test/tailtest.js
+++ test/tailtest.js
@@ -1,12 +1,25 @@
-let assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
 let tail = require('../tail')
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
+  it("returns 2 for result.length", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result.length, 2);
+  });
+  
+  it("returns 'Lighthouse' for result[0]", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result[0], "Lighthouse");
+  });
+  
+  it("returns 'Labs' for result[1]", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result[0], "Lighthouse");
+  });
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
-
+it("returns 3 for words.length", () => {
+  const words = ["Yo Yo", "Lighthouse", "Labs"];
+  tail(words);
+  assert.strictEqual(words.length, 3);
+});
+});
